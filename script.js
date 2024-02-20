ccbe5d4b3eb2012a370b582bc2618f2cddcceeb3
nikzaf <nikzaf10@gmail.com>
2024-02-20 08:14:59 +0200

    Edited JS and added imports into html files

--- script.js
+++ script.js
@@ -22,25 +22,21 @@ function showRemovedFromCartMessage() {
 
 // Function to handle product quantity changes in the cart
 function updateCartItemQuantity(itemId, newQuantity) {
-    // Here you could perform operations like updating the quantity in the cart
     console.log(`Updated quantity of item ${itemId} to ${newQuantity}`);
 }
 
 // Function to fetch recommended products based on user preferences
 function fetchRecommendedProducts(userId) {
-    // Here you could make an AJAX request to fetch recommended products
     console.log(`Fetching recommended products for user ${userId}`);
 }
 
 // Function to track user interactions with the website
 function trackUserInteractions(action) {
-    // Here you could send data to a tracking service or analytics platform
     console.log(`User performed action: ${action}`);
 }
 
 // Function to dynamically load additional content or features
 function loadAdditionalContent() {
-    // Here you could load additional content based on certain conditions or triggers
     console.log('Loading additional content...');
 }
 
